8758427feaf97968cfc9568fcd53580452b475cc
EmreMaltepe <emremaltepe54@outlook.com>
2025-01-29 23:30:22 +0300

    final project

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,7 @@
 import "./js/init.js";
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+import { images } from "../data/images";
 
 const images = [
   {
