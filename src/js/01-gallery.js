7b6755c9df2e4720c311de108ed5a6280f33a0df
EmreMaltepe <emremaltepe54@outlook.com>
2025-02-18 12:52:37 +0300

    new update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,5 @@
-import "../js/init";
-import { images } from ".data/images";
+import "./js/init";
+
 import SimpleLightbox from "simplelightbox";
 import "/node_modules/simplelightbox/dist/simple-lightbox.min.css";
 
